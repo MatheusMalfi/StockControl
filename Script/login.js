635b023355cb4981ff3f72784b199aeb5786ac64
Matheus Malfi Gonzaga <matheus.malfi@icloud.com>
2025-11-06 02:32:09 -0300

    (feat)login: Adiciona funcionalidade de login com validação e redirecionamento (BACKEND)

---
+++ Script/login.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form2 = document.getElementById('loginForm');
+
+    form2.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        console.log("Submit interceptado");
+        
+        const email = document.getElementById('email').value;
+        const senha = document.getElementById('senha').value;
+
+        try{
+        const response = await fetch('http://localhost:3000/api/login', {
+            method: "POST",
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({email, senha})
+        });
+
+        const respostaLog = await response.json();
+        console.log("Resposta do servidor: ", respostaLog);
+
+        if(respostaLog.success){
+            window.location.href = 'index.html';
+        } else{
+            alert("E-mail ou senha invalidos.");}
+        }catch(erro){
+            console.error("Error ao enviar: ", erro);
+            alert("Erro ao realizar Login.");
+        }
+    });
+});
