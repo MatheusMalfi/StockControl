6a3ae29630ea746abbbee056dd8fd6467fb3091e
Otavio Augusto Piccoli <totavio.piccoli@gmail.com>
2025-11-10 21:29:22 -0300

    fix: servidor package e lock atualizados com as rotas de api e npm install do mysql, express, cors

--- backend/server.js
+++ backend/server.js
@@ -3,20 +3,20 @@ const express = require('express');
 const mysql = require('mysql2/promise');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
-const path = require('path')
+const path = require('path');
+const Sequelize = require('sequelize');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-const PORT = 3000;
+const PORT = 3306;
 
-const pool = mysql.createPool({
-  host: 'localst',
-  user: 'root',
-  password: 'StockControl',
-  database: 'SC_db',
-  port: 3306
+const sequelize = new Sequelize("sc_db", "root", "!StockControl2025", {
+  host: 'localhost',
+  dialect: 'mysql'
 });
+
+module.exports = sequelize; 
 
 // tabelas Criadas:
 // ong_user
@@ -29,7 +29,6 @@ const pool = mysql.createPool({
   //  ->     celular VARCHAR(20) NOT NULL,
     //->     endereco VARCHAR(255),
 //    ->     criado_em DATETIME DEFAULT CURRENT_TIMESTAMP
-
 
 //CREATE TABLE items_cadastrados (
 //    id_item INT AUTO_INCREMENT PRIMARY KEY,
@@ -52,6 +51,12 @@ const pool = mysql.createPool({
 // 	img_file INT
 // );
 
+
+
+app.get('/', async (req, res) => {
+  res.send("funcionando");
+});
+
 app.post('/api/cadastro', async (req, res) =>{
   const{email, senha, nome, cnpj, endereco, telefone, celular} = req.body;
   const camposUnicos = {email, nome, cnpj, endereco, telefone, celular};
@@ -60,7 +65,7 @@ app.post('/api/cadastro', async (req, res) =>{
   //--**Validação de Duplicata**--
   for(const [campo, valor] of Object.entries(camposUnicos)){
     const query = `SELECT * FROM ong_user WHERE ${campo} = ? LIMIT 1`;
-    const [rowsDuplicata] = await pool.query(query, [valor]);
+    const [rowsDuplicata] = await sequelize.query(query, [valor]);
 
     if (rowsDuplicata.length > 0 ){
       return res.status(400).json({erro: `${campo} já está cadastrado`})
@@ -69,7 +74,7 @@ app.post('/api/cadastro', async (req, res) =>{
 
   //--**Cadastro no DB**--
   try{
-    const [result] = await pool.execute(
+    const [result] = await sequelize.execute(
       'INSERT INTO ong_user (email, senha_hash, nome, cnpj, endereco, telefone, celular) VALUES (?, ?, ?, ?, ?, ?, ?)',
       [email, hash, nome, cnpj, endereco, telefone, celular]
     );
@@ -87,7 +92,7 @@ app.post('/api/cadastro', async (req, res) =>{
 app.post('/api/login', async (request, response) => {
   const {email, senha} = request.body;
   try {
-    const [rows] = await pool.query('SELECT * FROM ong_user WHERE email = ?', [email]);
+    const [rows] = await sequelize.query('SELECT * FROM ong_user WHERE email = ?', [email]);
 
     if (rows.length === 0){
       return response.status(401).json({mensagem: 'E-mail ou senha incorretos.'});
